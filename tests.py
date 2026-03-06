c899ccd23406b10ec18d07d6ae05787c7918476a
peterqlin <peterql2@illinois.edu>
2026-03-06 14:16:47 -0600

    add test file with dummy test, need to rework engine to be able to effectively test

---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+from engine import NoLimitHoldemEngine
+
+class TestPokerEngine(unittest.TestCase):
+
+    def test_preflop(self):
+        # TODO: make it so you can input game state to engine and get out game state to compare with correct state
+        self.assertEqual(1,1)
+
+if __name__ == "__main__":
+    unittest.main()
